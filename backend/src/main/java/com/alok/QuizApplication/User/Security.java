b6c8984bc8fe7ff9d5dc59ad3a7b355761cef9d6
Alok2001MD <alokmattihalli@gmail.com>
2024-04-21 17:52:58 +0530

    security file

--- backend/src/main/java/com/alok/QuizApplication/User/Security.java
+++ backend/src/main/java/com/alok/QuizApplication/User/Security.java
@@ -1,19 +1,19 @@
 package com.alok.QuizApplication.User;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class Security extends WebSecurityConfigurerAdapter {
+public class Security implements WebMvcConfigurer {
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .cors() // Allow CORS
-                .and()
-                .csrf().disable()
-                .authorizeRequests()
-                .anyRequest().permitAll();
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true); // Allow credentials
     }
 }
